0d2bccff920be3669e0d72e9397ee1d8b80c5703
Dmitriy Saulov <saulovdmitriy@gmail.com>
2019-11-09 18:15:15 +0300

    Изменил название папки на App_twitter и выполнил 4 урок

---
+++ App-twitter/ls2/src/components/post-list-item/post-list-item.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+import './post-list-item.css';
+
+
+export default class PostListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            important: false,
+            like: false
+        }
+        this.onImportant = this.onImportant.bind(this);
+        this.onLike = this.onLike.bind(this);
+    }
+
+    onImportant() {
+        this.setState(({important}) => ({
+            important: !important
+        }))
+    }
+
+    onLike() {
+        this.setState(({like}) => ({
+            like: !like
+        }))
+    }
+
+    render () {
+        const {label} = this.props;
+        const {important, like} = this.state;
+        let classNames = 'app-list-item d-flex justify-content-between';
+
+        if (important) {
+            classNames += ' important';
+        }
+
+        if (like ){
+            classNames += ' like';
+        }
+
+        return (
+            <div className={classNames}>
+                <span 
+                className="app-list-item-label"
+                onClick={this.onLike}>
+                    {label}
+                </span>
+                <div className="d-flex justify-content-center align-items-center">
+                    <div className="date-now">
+                        17:14 04.11.19
+                    </div>
+                    <button 
+                        type="button" 
+                        className="btn-star btn-sm">
+                            <i className="fa fa-star"
+                            onClick={this.onImportant}></i>
+                    </button>
+                    <button 
+                        type="button" 
+                        className="btn-trash btn-sm">
+                            <i className="fa fa-trash-o"></i>
+                    </button>
+                    <i className="fa fa-heart"></i>
+                </div>
+            </div>
+        )
+    }
+}
